2ba41b65aedd562d739743bc2b8c646109728426
pestahub <andru.pestov@gmail.com>
2020-03-14 01:51:17 +0300

    init_make_catkin_ws

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/pesta/chess_arm/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/pesta/chess_arm/devel;/home/pesta/ros_gazebo_test/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/pesta/chess_arm/devel;/home/pesta/ros_gazebo_test/catkin_ws/devel;/opt/ros/melodic" != "" else []
---
+++ build/opencv_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pesta/chess_arm/src/opencv_pkg
+# Build directory: /home/pesta/chess_arm/build/opencv_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
